e08c5a364ccadf5a1cff08eaa76218e27b9d9853
boilik <fw27eh1c@gmail.com>
2026-05-12 22:49:45 +0300

    added layout for pages

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express=require('express');
 const hbs=require('hbs')
+const expressHbs=require('express-handlebars')
+
 const app=express();
 const port=3000;
-hbs.registerPartials(__dirname+"/views/partials")
 
+hbs.registerPartials(__dirname + "/views/partials")
+
+app.engine("hbs",expressHbs.engine({
+    layoutsDir:__dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'layout',
+    extname:'hbs',
+    }
+))
+app.set('view engine','hbs')
+
+app.use("/",function(req,resp){
+    resp.render("main.hbs")
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-app.get("/",function(req,resp){
-    resp.render("main.hbs")
-})
