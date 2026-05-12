c413d2a9ca072ca30d6b2d1c824f8b5f21d144df
boilik <fw27eh1c@gmail.com>
2026-05-12 22:30:13 +0300

    added hbs and partial views

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express=require('express');
+const hbs=require('hbs')
 const app=express();
 const port=3000;
+hbs.registerPartials(__dirname+"/views/partials")
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.get("/",function(req,resp){
+    resp.render("main.hbs")
+})
