3eb1948ddb6602c37ab3b3d3d810f4ab17ecc68b
boilik <fw27eh1c@gmail.com>
2026-05-13 00:22:38 +0300

    added styles for sidebar

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express=require('express');
-const hbs=require('hbs')
 const expressHbs=require('express-handlebars')
 
 const app=express();
 const port=3000;
-
-hbs.registerPartials(__dirname + "/views/partials")
 
 app.engine("hbs",expressHbs.engine({
     layoutsDir:__dirname + '/views/layouts',
@@ -14,11 +11,15 @@ app.engine("hbs",expressHbs.engine({
     extname:'hbs',
     }
 ))
+
+app.use(express.static(__dirname + '/public'));
 app.set('view engine','hbs')
 
 app.use("/",function(req,resp){
-    resp.render("main.hbs")
+    resp.render("main")
 })
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
