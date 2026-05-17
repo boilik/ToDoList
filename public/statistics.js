8fd061a7191090890eb1ee6fb4e4bd9a37ff2c78
boilik <fw27eh1c@gmail.com>
2026-05-17 05:51:23 +0300

    added line chart to see Dynamics of tasks that done this week

--- public/statistics.js
+++ public/statistics.js
@@ -33,3 +33,71 @@ new Chart(donut, {
     type: 'doughnut',
     data: data,
 })
+
+
+const line = document.getElementById('line')
+
+function getCurrentWeekDates(){
+    const today = new Date()
+
+    const day = today.getDay() === 0 ? 7 : today.getDay() // делаем воскресенье = 7
+    const startOfWeek = new Date(today)
+    startOfWeek.setDate(today.getDate() - (day - 1)) // понедельник
+
+    const result = []
+
+    for (let d = new Date(startOfWeek); d <= today; d.setDate(d.getDate() + 1)) {
+        const dayNum = d.getDate()
+        const month = d.toLocaleString('en-US', { month: 'short' })
+
+        result.push(`${dayNum} ${month}`)
+    }
+
+    return result
+}
+
+
+function getDoneTasksNumberForWeek(tasks){
+    const today = new Date()
+    const day = today.getDay() === 0 ? 7 : today.getDay()
+
+    const startOfWeek = new Date(today)
+    startOfWeek.setDate(today.getDate() - (day - 1))
+
+    const endOfWeek = new Date(startOfWeek)
+    endOfWeek.setDate(startOfWeek.getDate() + 6)
+
+    let dates = getCurrentWeekDates().map(item => item.split(' ')[0])
+
+    let result = Object.fromEntries(
+        dates.map(d => [d, 0])
+    )
+
+    tasks.forEach(task => {
+        let [taskYear, taskMonth, taskDay] = task.date.split('.').map(Number)
+
+        const taskDate = new Date(taskYear, taskMonth - 1, taskDay)
+
+        if (taskDate >= startOfWeek && taskDate <= endOfWeek && task.done) {
+            const dayStr = String(taskDay)
+
+            if (result.hasOwnProperty(dayStr)) {
+                result[dayStr]++
+            }
+        }
+    })
+
+    return Object.values(result)
+}
+
+
+const dataline={
+    labels:getCurrentWeekDates(),
+    datasets:[{
+        data: getDoneTasksNumberForWeek(tasks),
+    }]
+}
+new Chart (line,{
+    type:'line',
+    data: dataline,
+})
